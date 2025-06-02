70346c1baacdcb4ea36eef7d8b6bf362f1a7876c
MightyBurger <52584279+MightyBurger@users.noreply.github.com>
2025-06-01 21:20:21 -0600

    split off glutin_ctx

---
+++ src/gui/glutin_ctx.rs
@@ -0,0 +1,147 @@
+use glutin::prelude::NotCurrentGlContext;
+use std::num::NonZeroU32;
+use winit::raw_window_handle::HasWindowHandle as _;
+
+pub struct GlutinWindowContext {
+    window: winit::window::Window,
+    gl_context: glutin::context::PossiblyCurrentContext,
+    gl_display: glutin::display::Display,
+    gl_surface: glutin::surface::Surface<glutin::surface::WindowSurface>,
+}
+
+impl GlutinWindowContext {
+    pub unsafe fn new(event_loop: &winit::event_loop::ActiveEventLoop) -> Self {
+        use glutin::display::GetGlDisplay as _;
+        use glutin::display::GlDisplay as _;
+        use glutin::prelude::GlSurface as _;
+        let winit_window_builder = winit::window::WindowAttributes::default()
+            .with_inner_size(crate::gui::SIZE)
+            .with_resizable(false)
+            .with_enabled_buttons({
+                let mut enabled_buttons = winit::window::WindowButtons::all();
+                enabled_buttons.remove(winit::window::WindowButtons::MAXIMIZE);
+                enabled_buttons
+            })
+            .with_title(crate::gui::TITLE)
+            .with_fullscreen(None)
+            .with_maximized(false)
+            .with_window_level(winit::window::WindowLevel::AlwaysOnTop)
+            // .with_theme(Some(winit::window::Theme::Dark))
+            .with_visible(false);
+
+        let config_template_builder = glutin::config::ConfigTemplateBuilder::new()
+            .prefer_hardware_accelerated(None)
+            .with_depth_size(0)
+            .with_stencil_size(0)
+            .with_transparency(false);
+
+        log::debug!("trying to get gl_config");
+
+        let (mut window, gl_config) = glutin_winit::DisplayBuilder::new()
+            .with_preference(glutin_winit::ApiPreference::FallbackEgl)
+            .with_window_attributes(Some(winit_window_builder.clone()))
+            .build(
+                event_loop,
+                config_template_builder,
+                |mut config_iterator| {
+                    config_iterator.next().expect(
+                        "failed to find a matching configuration for creating glutin config",
+                    )
+                },
+            )
+            .expect("failed to create gl_config");
+
+        let gl_display = gl_config.display();
+        log::debug!("found gl_config: {:?}", &gl_config);
+
+        let raw_window_handle = window.as_ref().map(|w| {
+            w.window_handle()
+                .expect("failed to get window handle")
+                .as_raw()
+        });
+        log::debug!("raw window handle: {:?}", raw_window_handle);
+
+        let context_attributes =
+            glutin::context::ContextAttributesBuilder::new().build(raw_window_handle);
+
+        // by default, glutin will try to create a core opengl context. but, if it is not available, try to create a gl-es context using this fallback attributes
+        let fallback_context_attributes = glutin::context::ContextAttributesBuilder::new()
+            .with_context_api(glutin::context::ContextApi::Gles(None))
+            .build(raw_window_handle);
+
+        let not_current_gl_context = unsafe {
+            gl_display
+                .create_context(&gl_config, &context_attributes)
+                .unwrap_or_else(|_| {
+                    log::debug!("failed to create gl_context with attributes: {:?}. retrying with fallback context attributes: {:?}",
+                    &context_attributes, &fallback_context_attributes);
+                    gl_config.display().create_context(&gl_config, &fallback_context_attributes).expect("failed to create context even with fallback attributes")
+                })
+        };
+
+        let window = window.take().unwrap_or_else(|| {
+            log::debug!("window doesn't exist yet. creating one now with finalize_window");
+            glutin_winit::finalize_window(event_loop, winit_window_builder.clone(), &gl_config)
+                .expect("failed to finalize glutin window")
+        });
+
+        let (width, height): (u32, u32) = window.inner_size().into();
+        let width = NonZeroU32::new(width).unwrap_or(NonZeroU32::MIN);
+        let height = NonZeroU32::new(height).unwrap_or(NonZeroU32::MIN);
+        let surface_attributes =
+            glutin::surface::SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new()
+                .build(
+                    window
+                        .window_handle()
+                        .expect("failed to get window handle")
+                        .as_raw(),
+                    width,
+                    height,
+                );
+
+        let gl_surface = unsafe {
+            gl_display
+                .create_window_surface(&gl_config, &surface_attributes)
+                .unwrap()
+        };
+        log::debug!("surface created successfully: {gl_surface:?}. making context current");
+        let gl_context = not_current_gl_context.make_current(&gl_surface).unwrap();
+
+        gl_surface
+            .set_swap_interval(
+                &gl_context,
+                glutin::surface::SwapInterval::Wait(NonZeroU32::MIN),
+            )
+            .unwrap();
+        Self {
+            window,
+            gl_context,
+            gl_display,
+            gl_surface,
+        }
+    }
+
+    pub fn window(&self) -> &winit::window::Window {
+        &self.window
+    }
+
+    pub fn resize(&self, physical_size: winit::dpi::PhysicalSize<u32>) {
+        use glutin::surface::GlSurface as _;
+        if let (Ok(width), Ok(height)) = (
+            physical_size.width.try_into(),
+            physical_size.height.try_into(),
+        ) {
+            self.gl_surface.resize(&self.gl_context, width, height);
+        }
+    }
+
+    pub fn swap_buffers(&self) -> glutin::error::Result<()> {
+        use glutin::surface::GlSurface as _;
+        self.gl_surface.swap_buffers(&self.gl_context)
+    }
+
+    pub fn get_proc_address(&self, addr: &std::ffi::CStr) -> *const std::ffi::c_void {
+        use glutin::display::GlDisplay as _;
+        self.gl_display.get_proc_address(addr)
+    }
+}
--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,6 +1,8 @@
 // The following code is from here:
 // https://github.com/emilk/egui/blob/main/crates/egui_glow/examples/pure_glow.rs
 
+use glutin_ctx::GlutinWindowContext;
+
 const TITLE: &'static str = "Reemap";
 const SIZE: winit::dpi::LogicalSize<f64> = winit::dpi::LogicalSize {
     width: 800.0,
@@ -8,154 +10,10 @@ const SIZE: winit::dpi::LogicalSize<f64> = winit::dpi::LogicalSize {
 };
 const START_VISIBLE: bool = true;
 
-use glutin::prelude::NotCurrentGlContext;
-use std::{num::NonZeroU32, sync::Arc};
+mod glutin_ctx;
+
+use std::sync::Arc;
 use tray_icon::TrayIcon;
-use winit::raw_window_handle::HasWindowHandle as _;
-
-struct GlutinWindowContext {
-    window: winit::window::Window,
-    gl_context: glutin::context::PossiblyCurrentContext,
-    gl_display: glutin::display::Display,
-    gl_surface: glutin::surface::Surface<glutin::surface::WindowSurface>,
-}
-
-impl GlutinWindowContext {
-    unsafe fn new(event_loop: &winit::event_loop::ActiveEventLoop) -> Self {
-        use glutin::display::GetGlDisplay as _;
-        use glutin::display::GlDisplay as _;
-        use glutin::prelude::GlSurface as _;
-        let winit_window_builder = winit::window::WindowAttributes::default()
-            .with_inner_size(SIZE)
-            .with_resizable(false)
-            .with_enabled_buttons({
-                let mut enabled_buttons = winit::window::WindowButtons::all();
-                enabled_buttons.remove(winit::window::WindowButtons::MAXIMIZE);
-                enabled_buttons
-            })
-            .with_title(TITLE)
-            .with_fullscreen(None)
-            .with_maximized(false)
-            .with_window_level(winit::window::WindowLevel::AlwaysOnTop)
-            // .with_theme(Some(winit::window::Theme::Dark))
-            .with_visible(false);
-
-        let config_template_builder = glutin::config::ConfigTemplateBuilder::new()
-            .prefer_hardware_accelerated(None)
-            .with_depth_size(0)
-            .with_stencil_size(0)
-            .with_transparency(false);
-
-        log::debug!("trying to get gl_config");
-
-        let (mut window, gl_config) = glutin_winit::DisplayBuilder::new()
-            .with_preference(glutin_winit::ApiPreference::FallbackEgl)
-            .with_window_attributes(Some(winit_window_builder.clone()))
-            .build(
-                event_loop,
-                config_template_builder,
-                |mut config_iterator| {
-                    config_iterator.next().expect(
-                        "failed to find a matching configuration for creating glutin config",
-                    )
-                },
-            )
-            .expect("failed to create gl_config");
-
-        let gl_display = gl_config.display();
-        log::debug!("found gl_config: {:?}", &gl_config);
-
-        let raw_window_handle = window.as_ref().map(|w| {
-            w.window_handle()
-                .expect("failed to get window handle")
-                .as_raw()
-        });
-        log::debug!("raw window handle: {:?}", raw_window_handle);
-
-        let context_attributes =
-            glutin::context::ContextAttributesBuilder::new().build(raw_window_handle);
-
-        // by default, glutin will try to create a core opengl context. but, if it is not available, try to create a gl-es context using this fallback attributes
-        let fallback_context_attributes = glutin::context::ContextAttributesBuilder::new()
-            .with_context_api(glutin::context::ContextApi::Gles(None))
-            .build(raw_window_handle);
-
-        let not_current_gl_context = unsafe {
-            gl_display
-                .create_context(&gl_config, &context_attributes)
-                .unwrap_or_else(|_| {
-                    log::debug!("failed to create gl_context with attributes: {:?}. retrying with fallback context attributes: {:?}",
-                    &context_attributes, &fallback_context_attributes);
-                    gl_config.display().create_context(&gl_config, &fallback_context_attributes).expect("failed to create context even with fallback attributes")
-                })
-        };
-
-        let window = window.take().unwrap_or_else(|| {
-            log::debug!("window doesn't exist yet. creating one now with finalize_window");
-            glutin_winit::finalize_window(event_loop, winit_window_builder.clone(), &gl_config)
-                .expect("failed to finalize glutin window")
-        });
-
-        let (width, height): (u32, u32) = window.inner_size().into();
-        let width = NonZeroU32::new(width).unwrap_or(NonZeroU32::MIN);
-        let height = NonZeroU32::new(height).unwrap_or(NonZeroU32::MIN);
-        let surface_attributes =
-            glutin::surface::SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new()
-                .build(
-                    window
-                        .window_handle()
-                        .expect("failed to get window handle")
-                        .as_raw(),
-                    width,
-                    height,
-                );
-
-        let gl_surface = unsafe {
-            gl_display
-                .create_window_surface(&gl_config, &surface_attributes)
-                .unwrap()
-        };
-        log::debug!("surface created successfully: {gl_surface:?}. making context current");
-        let gl_context = not_current_gl_context.make_current(&gl_surface).unwrap();
-
-        gl_surface
-            .set_swap_interval(
-                &gl_context,
-                glutin::surface::SwapInterval::Wait(NonZeroU32::MIN),
-            )
-            .unwrap();
-        Self {
-            window,
-            gl_context,
-            gl_display,
-            gl_surface,
-        }
-    }
-
-    fn window(&self) -> &winit::window::Window {
-        &self.window
-    }
-
-    fn resize(&self, physical_size: winit::dpi::PhysicalSize<u32>) {
-        use glutin::surface::GlSurface as _;
-        if let (Ok(width), Ok(height)) = (
-            physical_size.width.try_into(),
-            physical_size.height.try_into(),
-        ) {
-            self.gl_surface.resize(&self.gl_context, width, height);
-        }
-    }
-
-    fn swap_buffers(&self) -> glutin::error::Result<()> {
-        use glutin::surface::GlSurface as _;
-        self.gl_surface.swap_buffers(&self.gl_context)
-    }
-
-    fn get_proc_address(&self, addr: &std::ffi::CStr) -> *const std::ffi::c_void {
-        use glutin::display::GlDisplay as _;
-        self.gl_display.get_proc_address(addr)
-    }
-}
 
 #[derive(Debug)]
 enum ReemapGuiEvent {
@@ -175,7 +33,6 @@ struct GlowApp<T: TrayApp> {
     gl: Option<Arc<glow::Context>>,
     egui_glow: Option<egui_glow::EguiGlow>,
     repaint_delay: std::time::Duration,
-    clear_color: [f32; 3],
     tray_icon: Option<TrayIcon>,
     app_data: T,
 }
@@ -188,7 +45,6 @@ impl<T: TrayApp> GlowApp<T> {
             gl: None,
             egui_glow: None,
             repaint_delay: std::time::Duration::MAX,
-            clear_color: [0.1, 0.1, 0.1],
             tray_icon: None,
             app_data: T::default(),
         }
