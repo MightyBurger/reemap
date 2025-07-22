6cf89c09dde304ca42d21ddff5e927421fe722b4
MightyBurger <52584279+MightyBurger@users.noreply.github.com>
2025-07-21 20:26:48 -0600

    center buttons

--- src/gui/reemapp/ui_main.rs
+++ src/gui/reemapp/ui_main.rs
@@ -10,52 +10,78 @@ use super::GuiMenu;
 use super::ReemApp;
 
 pub fn ui_main(ui: &mut egui::Ui, args: &mut ReemApp) {
+    use super::BUTTON_HEIGHT;
     use super::BUTTON_SIZE;
+    use super::BUTTON_WIDTH;
+    use egui_extras::{Size, StripBuilder};
 
-    ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-        if ui
-            .add_sized(BUTTON_SIZE, egui::Button::new("Add Profile"))
-            .clicked()
-        {
-            args.gui_local.new_profile_modal = EditProfileModalOpts {
-                modal_open: true,
-                name: String::from("New Profile"),
-                open_windows: query_windows::enumerate_open_windows(),
-                ..Default::default()
-            };
-        }
-        if ui
-            .add_sized(BUTTON_SIZE, egui::Button::new("Rearrange"))
-            .clicked()
-        {
-            args.gui_local.rearrange_profiles_modal.new_order = args.config.profiles.clone();
-            args.gui_local.rearrange_profiles_modal.modal_open = true;
-        }
-        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-            ui.add_space(super::SPACING);
-            egui::Frame::new()
-                .stroke(egui::Stroke {
-                    width: 1.0,
-                    color: egui::Color32::DARK_GRAY,
-                })
-                .inner_margin(4.0)
-                .corner_radius(4.0)
-                .show(ui, |ui| {
-                    if args.config.profiles.is_empty() {
-                        ui.centered_and_justified(|ui| {
-                            ui.style_mut().interaction.selectable_labels = false;
-                            ui.label("Add a profile to get started.");
+    StripBuilder::new(ui)
+        .size(Size::remainder())
+        .size(Size::initial(BUTTON_HEIGHT))
+        .vertical(|mut strip| {
+            strip.cell(|ui| {
+                ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+                    ui.add_space(super::SPACING);
+                    egui::Frame::new()
+                        .stroke(egui::Stroke {
+                            width: 1.0,
+                            color: egui::Color32::DARK_GRAY,
+                        })
+                        .inner_margin(4.0)
+                        .corner_radius(4.0)
+                        .show(ui, |ui| {
+                            if args.config.profiles.is_empty() {
+                                ui.centered_and_justified(|ui| {
+                                    ui.style_mut().interaction.selectable_labels = false;
+                                    ui.label("Add a profile to get started.");
+                                });
+                            } else {
+                                let profile_select = ui_enable_clickable_table(
+                                    ui,
+                                    &mut args.config.profiles,
+                                    "Profiles",
+                                );
+                                if let Some(profile_idx) = profile_select {
+                                    args.gui_local.menu = GuiMenu::Profile { profile_idx };
+                                }
+                            }
                         });
-                    } else {
-                        let profile_select =
-                            ui_enable_clickable_table(ui, &mut args.config.profiles, "Profiles");
-                        if let Some(profile_idx) = profile_select {
-                            args.gui_local.menu = GuiMenu::Profile { profile_idx };
-                        }
-                    }
                 });
+            });
+            strip.strip(|builder| {
+                builder
+                    .size(Size::remainder())
+                    .sizes(Size::initial(BUTTON_WIDTH), 2) // 2 buttons
+                    .size(Size::remainder())
+                    .horizontal(|mut strip| {
+                        strip.empty();
+                        strip.cell(|ui| {
+                            if ui
+                                .add_sized(BUTTON_SIZE, egui::Button::new("Add Profile"))
+                                .clicked()
+                            {
+                                args.gui_local.new_profile_modal = EditProfileModalOpts {
+                                    modal_open: true,
+                                    name: String::from("New Profile"),
+                                    open_windows: query_windows::enumerate_open_windows(),
+                                    ..Default::default()
+                                };
+                            }
+                        });
+                        strip.cell(|ui| {
+                            if ui
+                                .add_sized(BUTTON_SIZE, egui::Button::new("Rearrange"))
+                                .clicked()
+                            {
+                                args.gui_local.rearrange_profiles_modal.new_order =
+                                    args.config.profiles.clone();
+                                args.gui_local.rearrange_profiles_modal.modal_open = true;
+                            }
+                        });
+                        strip.empty();
+                    });
+            });
         });
-    });
 
     // ----- New profile modal -----
 
